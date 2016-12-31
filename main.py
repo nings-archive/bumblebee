c222a7487ec6cdcc7ea3636abe5e0448e61d2e2b
ningSP4 <ningyuan.sg@gmail.com>
2016-12-31 23:58:49 +0800

    Tweak key/mouse input functions into class with decorators
    Organised the left click, right click, alt ping, and ctrl alt ping
    functions into a class, with the use of @alt and @ctrl decorators.
    
    I was unable to organise the pygame.mixer.music functions into a
    class of their own: for some reason, this version of the code with
    the pygame functions within a class does not work at all. Outside,
    however, it functions as intended.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 import sys, time, json
 import pyautogui, pygame.mixer, keyboard
 
-# INITIALISATION
+
+# CONFIG INITIALISATION
+pygame.mixer.init()
 try:
     with open('config.json', 'r') as file:
         CONFIG = json.load(file)
@@ -12,29 +14,16 @@ except FileNotFoundError:
     time.sleep(5)
     sys.exit()
 
-class Music:
-    def __init__(self):
-        pygame.mixer.init()
-        self.EXIT = 'exit.ogg'
-        self.BUMBLEBEE = 'bumblebee.mp3'
-        pygame.mixer.music.load(self.BUMBLEBEE)
-        pygame.mixer.music.set_volume(0.5)
-        pygame.mixer.music.play(-1)  # -1 repeats indefinitely
-        pygame.mixer.music.pause()
-        self.is_playing = False
-        self.was_playing = False
-
-    def pause(self):
-        pygame.mixer.music.pause()
-
-    def resume(self):
-        pygame.mixer.music.unpause()
-
-    def exit(self):
-        pygame.mixer.music.stop()
-        pygame.mixer.music.load(self.EXIT)
-        pygame.mixer.music.play()
-        time.sleep(1)
+# MUSIC INITIALISATION
+global is_playing, was_playing
+is_playing = False
+was_playing = False
+BUMBLEBEE = 'bumblebee.mp3'
+EXIT = 'exit.ogg'
+pygame.mixer.music.load(BUMBLEBEE)
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+pygame.mixer.music.pause()
 
 class Key_Inputs:
     def ctrl(click):
@@ -67,33 +56,36 @@ class Key_Inputs:
         pyautogui.click()
 
 def main():
-    music = Music()
     key_inputs = Key_Inputs()
+    global is_playing, was_playing
 
     if keyboard.is_pressed(CONFIG['left_click']):
         key_inputs.left_click()
-        music.is_playing = True
+        is_playing = True
     elif keyboard.is_pressed(CONFIG['right_click']):
         key_inputs.right_click()
-        music.is_playing = True
+        is_playing = True
     elif keyboard.is_pressed(CONFIG['alt_ping']):
         key_inputs.alt_ping()
-        music.is_playing = True
+        is_playing = True
     elif keyboard.is_pressed(CONFIG['ctrl_alt_ping']):
         key_inputs.ctrl_alt_ping()
-        music.is_playing = True
+        is_playing = True
     elif keyboard.is_pressed(CONFIG['terminate']):
-        music.exit()
+        pygame.mixer.music.stop()
+        pygame.mixer.music.load(EXIT)
+        pygame.mixer.music.play()
+        time.sleep(1)
         sys.exit()
     else:
-        music.is_playing = False  # FIX THIS
+        is_playing = False
     
-    if (music.is_playing is not music.was_playing) and (music.is_playing is True):
-        music.resume()
-        music.was_playing = music.is_playing
-    elif (music.is_playing is not music.was_playing) and (music.is_playing is False):
-        music.pause()
-        music.was_playing = music.is_playing
+    if (is_playing is not was_playing) and (is_playing is True):
+        pygame.mixer.music.unpause()
+        was_playing = is_playing
+    elif (is_playing is not was_playing) and (is_playing is False):
+        pygame.mixer.music.pause()
+        was_playing = is_playing
 
     time.sleep(0.001)
 
@@ -113,3 +105,4 @@ if __name__ == '__main__':
 
     while True:
         main()
+
